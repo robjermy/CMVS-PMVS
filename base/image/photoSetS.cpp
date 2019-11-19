19330ac5189cef5dafc960f627e4393089437bd0
Rob Jermy <robjermy@gmail.com>
2019-11-19 08:56:25 +0200

    Refactored photosets - Formatting - Removed usings

--- base/image/photoSetS.cpp
+++ base/image/photoSetS.cpp
@@ -4,28 +4,21 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-using namespace std;
-using namespace Image;
+Image::CPhotoSetS::CPhotoSetS() {}
+Image::CPhotoSetS::~CPhotoSetS() {}
 
-CPhotoSetS::CPhotoSetS(void) {
-}
-
-CPhotoSetS::~CPhotoSetS() {
-}
-
-
-void CPhotoSetS::init(const std::vector<int>& images, const std::string prefix,
-                      const int maxLevel, const int size, const int alloc) {
+void Image::CPhotoSetS::init(const std::vector<int>& images, const std::string prefix, const int maxLevel, const int size, const int alloc) {
   _images = images;
   _num = (int)images.size();
-  
-  for (int i = 0; i < (int)images.size(); ++i)
+
+  for (int i = 0; i < (int)images.size(); ++i) {
     _dict[images[i]] = i;
-  
+  }
+
   _prefix = prefix;
-  _maxLevel = max(1, maxLevel);
+  _maxLevel = std::max(1, maxLevel);
   _photos.resize(_num);
-  cerr << "Reading images: " << flush;
+  std::cerr << "Reading images: " << std::flush;
   for (int index = 0; index < _num; ++index) {
     const int image = _images[index];
 
@@ -35,204 +28,178 @@ void CPhotoSetS::init(const std::vector<int>& images, const std::string prefix,
     sprintf(test1, "%svisualize/%08d.jpg", prefix.c_str(), image);
     sprintf(test2, "%svisualize/%08d.png", prefix.c_str(), image);
     sprintf(test3, "%svisualize/%08d.tiff", prefix.c_str(), image);
-    if (ifstream(test0) || ifstream(test1)
+    if (std::ifstream(test0) || std::ifstream(test1)
 #if defined(PMVS_HAVE_PNG)
-      || ifstream(test2)
+      || std::ifstream(test2)
 #endif
 #if defined(PMVS_HAVE_TIFF)
-      || ifstream(test3)
+      || std::ifstream(test3)
 #endif
-                          ) {
-      char name[1024], mname[1024], ename[1024], cname[1024];    
-      
+    ) {
+      char name[1024], mname[1024], ename[1024], cname[1024];
+
       // Set name
       sprintf(name, "%svisualize/%08d", prefix.c_str(), image);
       sprintf(mname, "%smasks/%08d", prefix.c_str(), image);
       sprintf(ename, "%sedges/%08d", prefix.c_str(), image);
       sprintf(cname, "%stxt/%08d.txt", prefix.c_str(), image);
-      
-      _photos[index].init(name, mname, ename, cname, _maxLevel);        
-      if (alloc)
+
+      _photos[index].init(name, mname, ename, cname, _maxLevel);
+      if (alloc) {
         _photos[index].alloc();
-      else
+      } else {
         _photos[index].alloc(1);
-      cerr << '*' << flush;
-    }
-    // try 4 digits
-    else {
-      char name[1024], mname[1024], ename[1024], cname[1024];    
-      
+      }
+      std::cerr << '*' << std::flush;
+    } else { // try 4 digits
+      char name[1024], mname[1024], ename[1024], cname[1024];
+
       // Set name
       sprintf(name, "%svisualize/%04d", prefix.c_str(), image);
       sprintf(mname, "%smasks/%04d", prefix.c_str(), image);
       sprintf(ename, "%sedges/%04d", prefix.c_str(), image);
       sprintf(cname, "%stxt/%04d.txt", prefix.c_str(), image);
-      
-      _photos[index].init(name, mname, ename, cname, _maxLevel);        
-      if (alloc)
+
+      _photos[index].init(name, mname, ename, cname, _maxLevel);
+      if (alloc) {
         _photos[index].alloc();
-      else
+      } else {
         _photos[index].alloc(1);
-      cerr << '*' << flush;
+      }
+      std::cerr << '*' << std::flush;
     }
-
-    /*
-    const int image = _images[index];
-    char name[1024], mname[1024], ename[1024], cname[1024];
-
-    // Set name
-    sprintf(name, "%svisualize/%08d", prefix.c_str(), image);
-    sprintf(mname, "%smasks/%08d", prefix.c_str(), image);
-    sprintf(ename, "%sedges/%08d", prefix.c_str(), image);
-    sprintf(cname, "%stxt/%08d.txt", prefix.c_str(), image);
-
-    _photos[index].init(name, mname, ename, cname, _maxLevel);        
-    if (alloc)
-      _photos[index].alloc();
-    else
-      _photos[index].alloc(1);
-    cerr << '*' << flush;
-    */
   }
-  cerr << endl;
+  std::cerr << std::endl;
   const int margin = size / 2;
   _size = 2 * margin + 1;
 }
 
-void CPhotoSetS::free(void) {
-  for (int index = 0; index < (int)_photos.size(); ++index)
+void Image::CPhotoSetS::free(void) {
+  for (int index = 0; index < (int)_photos.size(); ++index) {
     _photos[index].free();
+  }
 }
 
-void CPhotoSetS::free(const int level) {
-  for (int index = 0; index < (int)_photos.size(); ++index)
+void Image::CPhotoSetS::free(const int level) {
+  for (int index = 0; index < (int)_photos.size(); ++index) {
     _photos[index].free(level);
+  }
 }
 
-void CPhotoSetS::setEdge(const float threshold) {
-  for (int index = 0; index < _num; ++index)
+void Image::CPhotoSetS::setEdge(const float threshold) {
+  for (int index = 0; index < _num; ++index) {
     _photos[index].setEdge(threshold);
+  }
 }
 
-void CPhotoSetS::write(const std::string outdir) {
+void Image::CPhotoSetS::write(const std::string outdir) {
   for (int index = 0; index < _num; ++index) {
     const int image = _images[index];
     char buffer[1024];
     sprintf(buffer, "%s%08d.txt", outdir.c_str(), image);
-    
+
     _photos[index].write(buffer);
   }
 }
 
 // get x and y axis to collect textures given reference index and normal
-void CPhotoSetS::getPAxes(const int index, const Vec4f& coord, const Vec4f& normal,
-                          Vec4f& pxaxis, Vec4f& pyaxis) const{
+void Image::CPhotoSetS::getPAxes(const int index, const Vec4f& coord, const Vec4f& normal, Vec4f& pxaxis, Vec4f& pyaxis) const{
   _photos[index].getPAxes(coord, normal, pxaxis, pyaxis);
 }
 
-void CPhotoSetS::grabTex(const int index, const int level, const Vec2f& icoord,
-                         const Vec2f& xaxis, const Vec2f& yaxis,
-                         std::vector<Vec3f>& tex, const int normalizef) const{
+void Image::CPhotoSetS::grabTex(const int index, const int level, const Vec2f& icoord, const Vec2f& xaxis, const Vec2f& yaxis, std::vector<Vec3f>& tex, const int normalizef) const{
   _photos[index].grabTex(level, icoord, xaxis, yaxis, _size, tex, normalizef);
 }
 
 // grabTex given 3D sampling information
-void CPhotoSetS::grabTex(const int index, const int level, const Vec4f& coord,
-                         const Vec4f& pxaxis, const Vec4f& pyaxis, const Vec4f& pzaxis,
-                         std::vector<Vec3f>& tex, float& weight,
-                         const int normalizef) const {
-  _photos[index].grabTex(level, coord, pxaxis, pyaxis, pzaxis,
-                          _size, tex, weight, normalizef);
+void Image::CPhotoSetS::grabTex(const int index, const int level, const Vec4f& coord, const Vec4f& pxaxis, const Vec4f& pyaxis, const Vec4f& pzaxis, std::vector<Vec3f>& tex, float& weight, const int normalizef) const {
+  _photos[index].grabTex(level, coord, pxaxis, pyaxis, pzaxis, _size, tex, weight, normalizef);
 }
 
-float CPhotoSetS::incc(const std::vector<std::vector<Vec3f> >& texs,
-                       const std::vector<float>& weights) {
+float Image::CPhotoSetS::incc(const std::vector<std::vector<Vec3f> >& texs, const std::vector<float>& weights) {
   float incctmp = 0.0;
   float denom = 0.0;
   for (int i = 0; i < (int)weights.size(); ++i) {
-    if (texs[i].empty())
-      continue;
+    if (texs[i].empty()) continue;
+
     for (int j = i+1; j < (int)weights.size(); ++j) {
-      if (texs[j].empty())
-	continue;
-      
+      if (texs[j].empty()) continue;
+
       const float weight = weights[i] * weights[j];
       const float ftmp = CPhoto::idot(texs[i], texs[j]);
       incctmp += ftmp * weight;
       denom += weight;
     }
   }
-  
-  if (denom == 0.0)
+
+  if (denom == 0.0) {
     return 2.0f;
-  else
-    return incctmp / denom;
+  }
+
+  return incctmp / denom;
 }
 
-void CPhotoSetS::getMinMaxAngles(const Vec4f& coord, const std::vector<int>& indexes,
-                                 float& minAngle, float& maxAngle) const {
+void Image::CPhotoSetS::getMinMaxAngles(const Vec4f& coord, const std::vector<int>& indexes, float& minAngle, float& maxAngle) const {
   minAngle = M_PI;
   maxAngle = 0.0f;
-  vector<Vec4f> rays;  rays.resize((int)indexes.size());
+  std::vector<Vec4f> rays;  rays.resize((int)indexes.size());
   for (int i = 0; i < (int)indexes.size(); ++i) {
     const int index = indexes[i];
     rays[i] = _photos[index].OpticalCenter() - coord;
     unitize(rays[i]);
   }
-  
+
   for (int i = 0; i < (int)indexes.size(); ++i) {
     for (int j = i+1; j < (int)indexes.size(); ++j) {
-      const float dot = max(-1.0f, min(1.0f, rays[i] * rays[j]));
+      const float dot = std::max(-1.0f, std::min(1.0f, rays[i] * rays[j]));
       const float angle = acos(dot);
-      minAngle = min(angle, minAngle);
-      maxAngle = max(angle, maxAngle);
+      minAngle = std::min(angle, minAngle);
+      maxAngle = std::max(angle, maxAngle);
     }
   }
 }
 
-int CPhotoSetS::checkAngles(const Vec4f& coord,
-                            const std::vector<int>& indexes,
-                            const float minAngle, const float maxAngle,
-                            const int num) const {
+int Image::CPhotoSetS::checkAngles(const Vec4f& coord, const std::vector<int>& indexes, const float minAngle, const float maxAngle, const int num) const {
   int count = 0;
-  
-  vector<Vec4f> rays;  rays.resize((int)indexes.size());
+
+  std::vector<Vec4f> rays;  rays.resize((int)indexes.size());
   for (int i = 0; i < (int)indexes.size(); ++i) {
     const int index = indexes[i];
     rays[i] = _photos[index].OpticalCenter() - coord;
     unitize(rays[i]);
   }
-  
+
   for (int i = 0; i < (int)indexes.size(); ++i) {
     for (int j = i+1; j < (int)indexes.size(); ++j) {
-      const float dot = max(-1.0f, min(1.0f, rays[i] * rays[j]));
+      const float dot = std::max(-1.0f, std::min(1.0f, rays[i] * rays[j]));
       const float angle = acos(dot);
-      if (minAngle < angle && angle < maxAngle)
+      if (minAngle < angle && angle < maxAngle) {
         ++count;
+      }
     }
   }
 
-  //if (count < num * (num - 1) / 2)
-  if (count < 1)
+  if (count < 1) {
     return 1;
-  else
-    return 0;
+  }
+
+  return 0;
 }
 
-float CPhotoSetS::computeDepth(const int index, const Vec4f& coord) const {
+float Image::CPhotoSetS::computeDepth(const int index, const Vec4f& coord) const {
   return _photos[index].computeDepth(coord);
 }
 
-void CPhotoSetS::setDistances(void) {
+void Image::CPhotoSetS::setDistances(void) {
   _distances.resize(_num);
   float avedis = 0.0f;
   int denom = 0;
   for (int i = 0; i < _num; ++i) {
     _distances[i].resize(_num);
     for (int j = 0; j < _num; ++j) {
-      if (i == j)
+      if (i == j) {
         _distances[i][j] = 0.0f;
-      else {
+      } else {
         const float ftmp = norm(_photos[i].OpticalCenter() - _photos[j].OpticalCenter());
         _distances[i][j] = ftmp;
         avedis += ftmp;
@@ -240,15 +207,15 @@ void CPhotoSetS::setDistances(void) {
       }
     }
   }
-  if (denom == 0)
-    return;
-  
+
+  if (denom == 0) return;
+
   avedis /= denom;
   if (avedis == 0.0f) {
-    cerr << "All the optical centers are identical..?" << endl;
+    std::cerr << "All the optical centers are identical..?" << std::endl;
     exit (1);
   }
-  
+
   // plus angle difference
   for (int i = 0; i < _num; ++i) {
     Vec4f ray0 = _photos[i].OpticalAxis();
@@ -256,19 +223,20 @@ void CPhotoSetS::setDistances(void) {
     for (int j = 0; j < _num; ++j) {
       Vec4f ray1 = _photos[j].OpticalAxis();
       ray1[3] = 0.0f;
-      
+
       _distances[i][j] /= avedis;
       const float margin = cos(10.0f * M_PI / 180.0f);
-      const float dis = max(0.0f, 1.0f - ray0 * ray1 - margin);
+      const float dis = std::max(0.0f, 1.0f - ray0 * ray1 - margin);
       _distances[i][j] += dis;
     }
   }
 }
 
-int CPhotoSetS::image2index(const int image) const {
-  map<int, int>::const_iterator pos = _dict.find(image);
-  if (pos == _dict.end())
+int Image::CPhotoSetS::image2index(const int image) const {
+  std::map<int, int>::const_iterator pos = _dict.find(image);
+  if (pos == _dict.end()) {
     return -1;
-  else
-    return pos->second;
+  }
+
+  return pos->second;
 }
--- base/image/photoSetS.hpp
+++ base/image/photoSetS.hpp
@@ -1,171 +1,138 @@
-#ifndef IMAGE_PHOTOSETS_H
-#define IMAGE_PHOTOSETS_H
+#pragma once
 
 #include <map>
+
 #include "photo.hpp"
 
 namespace Image {
+  class CPhotoSetS {
+  public:
+    CPhotoSetS();
+    virtual ~CPhotoSetS();
 
-class CPhotoSetS {
- public:
-  CPhotoSetS(void);
-  virtual ~CPhotoSetS();
+    void init(const std::vector<int>& images, const std::string prefix, const int maxLevel, const int size, const int alloc);
 
-  void init(const std::vector<int>& images, const std::string prefix,
-            const int maxLevel, const int size, const int alloc);
-  
-  // grabTex given 2D sampling information
-  void grabTex(const int index, const int level, const Vec2f& icoord,
-	       const Vec2f& xaxis, const Vec2f& yaxis,
-	       std::vector<Vec3f>& tex, const int normalizef = 1) const;
+    // grabTex given 2D sampling information
+    void grabTex(const int index, const int level, const Vec2f& icoord, const Vec2f& xaxis, const Vec2f& yaxis, std::vector<Vec3f>& tex, const int normalizef = 1) const;
 
-  // grabTex given 3D sampling information
-  void grabTex(const int index, const int level, const Vec4f& coord,
-	       const Vec4f& pxaxis, const Vec4f& pyaxis, const Vec4f& pzaxis,
-	       std::vector<Vec3f>& tex, float& weight,
-               const int normalizef = 1) const;
-  
-  void write(const std::string outdir);
-  void free(void);
-  void free(const int level);
+    // grabTex given 3D sampling information
+    void grabTex(const int index, const int level, const Vec4f& coord, const Vec4f& pxaxis, const Vec4f& pyaxis, const Vec4f& pzaxis, std::vector<Vec3f>& tex, float& weight, const int normalizef = 1) const;
 
-  void setEdge(const float threshold);
+    void write(const std::string outdir);
+    void free(void);
+    void free(const int level);
 
-  inline Vec3f project(const int index, const Vec4f& coord, const int level) const;
-  inline Vec3f mult(const int index, const Vec4f& coord, const int level) const;
-  
-  inline int getWidth(const int index, const int level) const;
-  inline int getHeight(const int index, const int level) const;
+    void setEdge(const float threshold);
 
-  inline Vec3f getColor(const Vec4f& coord, const int index,
-                        const int level) const;
-  inline Vec3f getColor(const int index, const float fx, const float fy,
-                        const int level) const;
-  inline Vec3f getColor(const int index, const int ix, const int iy,
-                        const int level) const;
-  
-  inline int getMask(const Vec4f& coord, const int level) const;
-  inline int getMask(const Vec4f& coord, const int index, const int level) const;
-  inline int getMask(const int index, const float fx, const float fy,
-                     const int level) const;
-  inline int getMask(const int index, const int ix, const int iy,
-                     const int level) const;
+    inline Vec3f project(const int index, const Vec4f& coord, const int level) const;
+    inline Vec3f mult(const int index, const Vec4f& coord, const int level) const;
 
-  inline int getEdge(const Vec4f& coord, const int index, const int level) const;
-  inline int getEdge(const int index, const float fx, const float fy,
-                     const int level) const;
-  inline int getEdge(const int index, const int ix, const int iy,
-                     const int level) const;
-  
-  static float incc(const std::vector<std::vector<Vec3f> >& texs,
-		    const std::vector<float>& weights);
+    inline int getWidth(const int index, const int level) const;
+    inline int getHeight(const int index, const int level) const;
 
-  int checkAngles(const Vec4f& coord, const std::vector<int>& indexes,
-                  const float minAngle, const float maxAngle,
-                  const int tau) const;
-  
-  void getMinMaxAngles(const Vec4f& coord, const std::vector<int>& indexes,
-                       float& minAngle, float& maxAngle) const;
+    inline Vec3f getColor(const Vec4f& coord, const int index, const int level) const;
+    inline Vec3f getColor(const int index, const float fx, const float fy, const int level) const;
+    inline Vec3f getColor(const int index, const int ix, const int iy, const int level) const;
 
-  float computeDepth(const int index, const Vec4f& coord) const;
-  
-  // Take care of indexes
-  std::vector<int> _images;
-  std::vector<CPhoto> _photos;
+    inline int getMask(const Vec4f& coord, const int level) const;
+    inline int getMask(const Vec4f& coord, const int index, const int level) const;
+    inline int getMask(const int index, const float fx, const float fy, const int level) const;
+    inline int getMask(const int index, const int ix, const int iy, const int level) const;
 
-  int image2index(const int image) const;
-  std::map<int, int> _dict;
-  
-  // Number of cameras.
-  int _num;
-  // Root directory
-  std::string _prefix;
-  // maximum level
-  int _maxLevel;
-  // Window size used to refine location
-  int _size;
+    inline int getEdge(const Vec4f& coord, const int index, const int level) const;
+    inline int getEdge(const int index, const float fx, const float fy, const int level) const;
+    inline int getEdge(const int index, const int ix, const int iy, const int level) const;
 
-  // getPAxes
-  void getPAxes(const int index, const Vec4f& coord, const Vec4f& normal,
-		Vec4f& pxaxis, Vec4f& pyaxis) const;
+    static float incc(const std::vector<std::vector<Vec3f> >& texs, const std::vector<float>& weights);
 
-  // pairwise distance based on optical center and viewing direction
-  void setDistances(void);
-  std::vector<std::vector<float> > _distances;
- protected:  
-}; 
- 
-Vec3f CPhotoSetS::project(const int index, const Vec4f& coord,
-                                    const int level) const{
-  return _photos[index].project(coord, level);
-};
+    int checkAngles(const Vec4f& coord, const std::vector<int>& indexes, const float minAngle, const float maxAngle, const int tau) const;
 
-Vec3f CPhotoSetS::mult(const int index, const Vec4f& coord,
-                                    const int level) const{
-  return _photos[index].mult(coord, level);
-};
- 
-int CPhotoSetS::getWidth(const int index, const int level) const {
-  return _photos[index].getWidth(level);
-};
- 
-int CPhotoSetS::getHeight(const int index, const int level) const {
-  return _photos[index].getHeight(level);
-};
+    void getMinMaxAngles(const Vec4f& coord, const std::vector<int>& indexes, float& minAngle, float& maxAngle) const;
 
-Vec3f CPhotoSetS::getColor(const Vec4f& coord, const int index,
-                          const int level) const {
-  return _photos[index].getColor(coord, level);
-};
- 
-Vec3f CPhotoSetS::getColor(const int index, const float fx, const float fy,
-                          const int level) const {
-  return _photos[index].Image::CImage::getColor(fx, fy, level);
-};
- 
-Vec3f CPhotoSetS::getColor(const int index, const int ix, const int iy,
-                          const int level) const {
-  return _photos[index].Image::CImage::getColor(ix, iy, level);
-};
- 
-int CPhotoSetS::getMask(const Vec4f& coord, const int level) const {
-  for (int index = 0; index < _num; ++index)
-    if (getMask(coord, index, level) == 0)
-      return 0;
-  return 1;
-};
- 
-int CPhotoSetS::getMask(const Vec4f& coord, const int index,
-                       const int level) const {
-  return _photos[index].getMask(coord, level);
-};
- 
-int CPhotoSetS::getMask(const int index, const float fx, const float fy,
-                       const int level) const {
-  return _photos[index].Image::CImage::getMask(fx, fy, level);
-};
+    float computeDepth(const int index, const Vec4f& coord) const;
 
-int CPhotoSetS::getMask(const int index, const int ix, const int iy,
-                       const int level) const {
-  return _photos[index].Image::CImage::getMask(ix, iy, level); 
-};
+    // Take care of indexes
+    std::vector<int> _images;
+    std::vector<CPhoto> _photos;
 
-int CPhotoSetS::getEdge(const Vec4f& coord, const int index,
-                       const int level) const {
-  return _photos[index].getEdge(coord, level);
-};
- 
-int CPhotoSetS::getEdge(const int index, const float fx, const float fy,
-                       const int level) const {
-  return _photos[index].Image::CImage::getEdge(fx, fy, level);
-};
+    int image2index(const int image) const;
+    std::map<int, int> _dict;
 
-int CPhotoSetS::getEdge(const int index, const int ix, const int iy,
-                       const int level) const {
-  return _photos[index].Image::CImage::getEdge(ix, iy, level); 
-};
- 
-};
+    // Number of cameras.
+    int _num;
+    // Root directory
+    std::string _prefix;
+    // maximum level
+    int _maxLevel;
+    // Window size used to refine location
+    int _size;
 
-#endif // IMAGE_PHOTOSETS_H
-  
+    // getPAxes
+    void getPAxes(const int index, const Vec4f& coord, const Vec4f& normal, Vec4f& pxaxis, Vec4f& pyaxis) const;
+
+    // pairwise distance based on optical center and viewing direction
+    void setDistances(void);
+    std::vector<std::vector<float> > _distances;
+  };
+
+  Vec3f CPhotoSetS::project(const int index, const Vec4f& coord, const int level) const{
+    return _photos[index].project(coord, level);
+  };
+
+  Vec3f CPhotoSetS::mult(const int index, const Vec4f& coord, const int level) const{
+    return _photos[index].mult(coord, level);
+  };
+
+  int CPhotoSetS::getWidth(const int index, const int level) const {
+    return _photos[index].getWidth(level);
+  };
+
+  int CPhotoSetS::getHeight(const int index, const int level) const {
+    return _photos[index].getHeight(level);
+  };
+
+  Vec3f CPhotoSetS::getColor(const Vec4f& coord, const int index, const int level) const {
+    return _photos[index].getColor(coord, level);
+  };
+
+  Vec3f CPhotoSetS::getColor(const int index, const float fx, const float fy, const int level) const {
+    return _photos[index].Image::CImage::getColor(fx, fy, level);
+  };
+
+  Vec3f CPhotoSetS::getColor(const int index, const int ix, const int iy, const int level) const {
+    return _photos[index].Image::CImage::getColor(ix, iy, level);
+  };
+
+  int CPhotoSetS::getMask(const Vec4f& coord, const int level) const {
+    for (int index = 0; index < _num; ++index) {
+      if (getMask(coord, index, level) == 0) {
+        return 0;
+      }
+    }
+    return 1;
+  };
+
+  int CPhotoSetS::getMask(const Vec4f& coord, const int index, const int level) const {
+    return _photos[index].getMask(coord, level);
+  };
+
+  int CPhotoSetS::getMask(const int index, const float fx, const float fy, const int level) const {
+    return _photos[index].Image::CImage::getMask(fx, fy, level);
+  };
+
+  int CPhotoSetS::getMask(const int index, const int ix, const int iy, const int level) const {
+    return _photos[index].Image::CImage::getMask(ix, iy, level);
+  };
+
+  int CPhotoSetS::getEdge(const Vec4f& coord, const int index, const int level) const {
+    return _photos[index].getEdge(coord, level);
+  };
+
+  int CPhotoSetS::getEdge(const int index, const float fx, const float fy, const int level) const {
+    return _photos[index].Image::CImage::getEdge(fx, fy, level);
+  };
+
+  int CPhotoSetS::getEdge(const int index, const int ix, const int iy, const int level) const {
+    return _photos[index].Image::CImage::getEdge(ix, iy, level);
+  };
+}
